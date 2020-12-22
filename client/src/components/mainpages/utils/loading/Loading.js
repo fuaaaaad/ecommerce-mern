4c73948a9bd55593bd5decbe8f2f507b7f7933e9
Muhammed Fuaad <57677134+fuaaaaad@users.noreply.github.com>
2020-12-22 10:30:56 +0400

    Add files via upload

---
+++ client/src/components/mainpages/utils/loading/Loading.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './loading.scss'
+
+function Loading() {
+    return (
+        <div class="wrap">
+        <div class="loading">
+          <div class="bounceball"></div>
+          <div class="text">NOW LOADING</div>
+        </div>
+      </div>
+    )
+}
+
+export default Loading
